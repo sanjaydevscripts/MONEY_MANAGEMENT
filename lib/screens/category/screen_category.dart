75de99ff7d5ba750bc36d9e724e1566e656efc4c
sanjay <sanjayp9072@gmail.com>
2024-07-03 20:27:40 +0530

    tab added

--- lib/screens/category/screen_category.dart
+++ lib/screens/category/screen_category.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
 
-class ScreenCategory extends StatelessWidget {
+class ScreenCategory extends StatefulWidget {
   const ScreenCategory({super.key});
 
   @override
+  State<ScreenCategory> createState() => _ScreenCategoryState();
+}
+
+class _ScreenCategoryState extends State<ScreenCategory> with SingleTickerProviderStateMixin{
+
+late TabController _tabController;
+
+  @override
+  void initState() {
+   _tabController = TabController(length: 2, vsync:this);
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Category'));
+    return  Column(
+      children: [
+        TabBar(
+          controller: _tabController,
+          tabs: [
+          Tab(text: 'INCOME',),
+          Tab(text: 'EXPENSE',)
+        ])
+      ],
+    );
   }
 }
